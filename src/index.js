68581f385795534d5e7fb28abd3b45416674b689
Shumigai Natalia <shumigai_frontend@mail.ru>
2023-01-12 12:32:52 +0300

    fix: fix bug

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 module.exports = function reverse (n) {
-        n = n + "";
-        return  n.split("").reverse().join("");
+    return parseInt(n.toString().split('').reverse().join(''));
     }
-
-    console.log(reverse(123));
-    console.log(reverse(233));
-    console.log(reverse(535));
-    console.log(reverse(95034));
